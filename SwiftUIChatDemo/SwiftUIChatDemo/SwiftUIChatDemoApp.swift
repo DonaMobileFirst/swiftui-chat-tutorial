7a3827a08fa514ae353794a4bca4da573f968731
Dona <dona.mobilefirst@gmail.com>
2023-03-09 12:13:02 +0530

    comment added for test

--- SwiftUIChatDemo/SwiftUIChatDemo/SwiftUIChatDemoApp.swift
+++ SwiftUIChatDemo/SwiftUIChatDemo/SwiftUIChatDemoApp.swift
@@ -12,6 +12,7 @@ import StreamChatSwiftUI
 @main
 struct SwiftUIChatDemoApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    //This is a test for the merging process works fine or not.
     
     var body: some Scene {
         WindowGroup {
